14e21213c3e03afb0e8a2ca0ea3f4dfbfe3707c3
Huang Bo <huangbo@memoright.com>
2013-11-15 09:19:53 -0500

    Add ipython

--- init.el
+++ init.el
@@ -128,6 +128,25 @@
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
 
+;; ipython
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args ""
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+ "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+ "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+ "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
+(if (equal system-type 'windows-nt)
+    (setq
+     python-shell-interpreter "python"
+     python-shell-interpreter-args
+     "-i d:\\Python27\\Scripts\\ipython-script.py"))
+
 ;; cscope
 (unless (equal system-type 'windows-nt)
 	(require 'xcscope)
