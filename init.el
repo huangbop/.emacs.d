cae624a218f8c799635268abae086a51e84482a2
Huang Bo <huangbop@gmail.com>
2014-04-23 18:37:12 +0800

    Basis

--- init.el
+++ init.el
@@ -1,19 +1,51 @@
 ;;; Huang Bo's emacs configuration
 ;;; https://github.com/huangbop/.emacs.d
 
-;; ELPA 
+;; elpa
 (require 'package)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
 (package-initialize)
 
 (unless (file-exists-p "~/.emacs.d/elpa/archives/melpa")
   (package-refresh-contents))
 
-;; Ido vertical mode
-(package-install 'ido-vertical-mode)
+;; ido 
 (ido-mode 1)
+
+(package-install 'ido-vertical-mode)
+(require 'ido-vertical-mode)
 (ido-vertical-mode 1)
+
+(package-install 'flx-ido)
+(require 'flx-ido)
+(flx-ido-mode 1)
+
+;; window numbering
+(package-install 'window-numbering)
+(require 'window-numbering)
+(window-numbering-mode 1)
+
+;; smex
+(package-install 'smex)
+(require 'smex)
+(global-set-key (kbd "M-x") 'smex)
+
+;; frame basis
+(setq use-file-dialog nil)
+(setq use-dialog-box nil)
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-echo-area-message t)
+
+(menu-bar-mode -1)
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'set-scroll-bar-mode)
+  (set-scroll-bar-mode nil))
+
+;; edit basis
+(blink-cursor-mode -1)
+(column-number-mode)
 
 ;;; init.el ends here
