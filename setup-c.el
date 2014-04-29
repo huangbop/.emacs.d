fd15c612fc58c0f72dcf3addb78a69d51df64aed
Huang Bo <huangbop@gmail.com>
2014-04-29 16:25:34 +0800

    GUD keymap

--- setup-c.el
+++ setup-c.el
@@ -15,6 +15,19 @@
 (global-semantic-idle-summary-mode 1)
 (global-semantic-idle-completions-mode 1)
 
+;; gud
+(global-set-key (kbd "<f5>") 'gud-cont)
+(global-set-key (kbd "<f7>") 'gud-up)
+(global-set-key (kbd "<f8>") 'gud-down)
+(global-set-key (kbd "<f9>") 'gud-break)
+(global-set-key (kbd "<M-f9>") 'gud-tbreak)
+(global-set-key (kbd "<S-f9>") 'gud-remove)
+(global-set-key (kbd "<f10>") 'gud-next)
+(global-set-key (kbd "<C-f10>") 'gud-until)
+(global-set-key (kbd "<S-f10>") 'gud-finish)
+(global-set-key (kbd "<f11>") 'gud-step)
+(global-set-key (kbd "<f12>") 'gdb-many-windows)
+
 (provide 'setup-c)
 
 ;;; setup-c.el ends here
