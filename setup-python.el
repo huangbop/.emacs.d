e408a4c0124dc99a1f7266bbf04da992d5e546cc
Huang Bo <huangbop@gmail.com>
2014-07-08 14:09:44 +0800

    IPython

--- setup-python.el
+++ setup-python.el
@@ -1,5 +1,6 @@
 
 
+;; jedi
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t
       jedi:use-shortcuts t)
@@ -7,5 +8,24 @@
 (eval-after-load "python"
   '(define-key python-mode-map "\C-cx" 'jedi-direx:pop-to-buffer))
 (add-hook 'jedi-mode-hook 'jedi-direx:setup)
+
+;; use ipython
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args ""
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+ "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+ "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+ "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
+(if (equal system-type 'windows-nt)
+    (setq
+     python-shell-interpreter "python"
+     python-shell-interpreter-args "-i d:/Python27/Scripts/ipython-script.py"))
+
 
 (provide 'setup-python)
