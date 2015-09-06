febf9c3290e7316017969d9b4ac180fcb682f44a
Huang Bo <huangbop@gmail.com>
2015-09-07 03:56:19 +0800

    Init ignore

---
+++ init.el
@@ -0,0 +1,11 @@
+;;; init.el --- Emacs Initialization File
+;;
+;; Copyright (c) 2015 Huang Bo
+;;
+;; Author: Huang Bo <huangbop@gmail.com>
+;; URL: https://github.com/huangbop/.emacs.d
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
