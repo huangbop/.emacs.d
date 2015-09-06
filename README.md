a3d8d9c92c7134d411edf57cb6904d0fe7f91785
Huang Bo <huangbop@gmail.com>
2015-09-07 03:47:29 +0800

    Clear all

--- README.md
+++
@@ -1,4 +0,0 @@
-.emacs.d
-========
-
-My personal emacs settings.
--- modules/setup-java.el
+++
@@ -1,7 +0,0 @@
-;;; Indent
-(add-hook 'java-mode-hook (lambda ()
-			    (setq c-basic-offset 2)))
-
-
-(provide 'setup-java)
-;;; setup-java.el ends here
